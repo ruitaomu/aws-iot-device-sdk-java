17dc5c45abcfec63ab296eab0ca20b49e7cdd765
M@x <ruitao.mu@gmail.com>
2020-09-21 18:23:36 +0800

    Better logger

--- aws-iot-device-sdk-java/src/main/java/com/amazonaws/services/iot/client/logging/Level.java
+++ aws-iot-device-sdk-java/src/main/java/com/amazonaws/services/iot/client/logging/Level.java
@@ -1,7 +1,13 @@
 package com.amazonaws.services.iot.client.logging;
 
 public class Level {
-
-	public static final int WARNING = 1;
-
+	public static final int OFF = Integer.MAX_VALUE;
+	public static final int SEVERE = 1000;
+	public static final int WARNING = 800;
+	public static final int INFO = 700;
+	public static final int CONFIG = 600;
+	public static final int FINE = 500;
+	public static final int FINER = 400;
+	public static final int FINEST = 300;
+	public static final int ALL = Integer.MIN_VALUE;
 }
--- aws-iot-device-sdk-java/src/main/java/com/amazonaws/services/iot/client/logging/Logger.java
+++ aws-iot-device-sdk-java/src/main/java/com/amazonaws/services/iot/client/logging/Logger.java
@@ -1,41 +1,152 @@
 package com.amazonaws.services.iot.client.logging;
 
+import java.util.Hashtable;
+
 public class Logger {
 	
-	private static Logger logger = null;
+	private final static Hashtable loggers = new Hashtable();
+	
+	private final static int DEFAULT_LOGGING_LEVEL = Level.WARNING;
 
 	public synchronized static Logger getLogger(String name) {
-		if (logger == null) {
-			logger = new Logger(name);
+		if (loggers.containsKey(name)) {
+			return (Logger)loggers.get(name);
 		}
+		
+		Logger logger = new Logger(name);
+		logger.level = DEFAULT_LOGGING_LEVEL;
+		loggers.put(name, logger);
+		
 		return logger;
 	}
 
 	private String name;
+	
+	private int level;
 
 	protected Logger(String name) {
 		this.name = name;
 	}
+	
+	public void setLevel(int newLevel) {
+		level = newLevel;
+	}
+
+	private void severe(String message, Throwable e) {
+		if (level <= Level.SEVERE) {
+			System.out.println("[CRIT]<"+name+">"+message);
+			if (e != null) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void info(String message, Throwable e) {
+		if (level <= Level.INFO) {
+			System.out.println("[INFO]<"+name+">"+message);
+			if (e != null) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void warning(String message, Throwable e) {
+		if (level <= Level.WARNING) {
+			System.out.println("[WARN]<"+name+">"+message);
+			if (e != null) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void config(String message, Throwable e) {
+		if (level <= Level.CONFIG) {
+			System.out.println("[CONF]<"+name+">"+message);
+			if (e != null) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void fine(String message, Throwable e) {
+		if (level <= Level.FINE) {
+			System.out.println("[FINE]<"+name+">"+message);
+			if (e != null) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void finer(String message, Throwable e) {
+		if (level <= Level.FINER) {
+			System.out.println("[FINER]<"+name+">"+message);
+			if (e != null) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void finest(String message, Throwable e) {
+		if (level <= Level.FINEST) {
+			System.out.println("[FINEST]<"+name+">"+message);
+			if (e != null) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void severe(String message) {
+		severe(message, null);
+	}
 
 	public void info(String message) {
-		System.out.println("[INFO]"+message);
+		info(message, null);
 	}
 
 	public void warning(String message) {
-		System.out.println("[WARN]"+message);
+		warning(message, null);
+	}
+	
+	public void config(String message) {
+		config(message, null);
 	}
 
 	public void fine(String message) {
-		System.out.println("[FINE]"+message);
+		fine(message, null);
+	}
+	
+	public void finer(String message) {
+		finer(message, null);
+	}
+	
+	public void finest(String message) {
+		finest(message, null);
 	}
 
+	
 	public void log(int level, String message, Throwable e) {
-		System.out.println("[Level"+level+"]"+message);
-		System.out.println("<Stack>:");
-		e.printStackTrace();
-	}
-
-	public void error(String message) {
-		System.out.println("[CRIT]"+message);
+		switch (level) {
+		case Level.SEVERE:
+			severe(message, e);
+			break;
+		case Level.WARNING:
+			warning(message, e);
+			break;
+		case Level.INFO:
+			info(message, e);
+			break;
+		case Level.CONFIG:
+			config(message, e);
+			break;
+		case Level.FINE:
+			fine(message, e);
+			break;
+		case Level.FINER:
+			finer(message, e);
+			break;
+		case Level.FINEST:
+			finest(message, e);
+			break;
+		}		
 	}
 }
